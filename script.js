e8b719b436a58dbd0d4ec8fac4c4f3e717d0a3f9
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-20 10:52:03 +0100

    Adding scoreboard -- adding css styling (minimal)

--- script.js
+++ script.js
@@ -10,9 +10,18 @@
 
    const play = document.querySelector('.play');
 
+   // Adding minimum score (=1)
+   let pcNum = 1;
+   let playerNum = 1;
+
    // empty, to be able to fill in later with the player's choice
    let playerPick = "";                                        
    let result = "";
+
+   // The score that is displayed for both parties
+   let pcScoreboard = "";
+   let playerScoreboard = "";
+
    
    
    
@@ -82,9 +91,23 @@
          //console.log(result);
          
       };
+
+
       
-      document.querySelector('.answer').innerHTML = computerPick,
+      document.querySelector('.answer').innerHTML = computerPick;
       document.querySelector('.result').innerHTML = result;
+
+
+      // if statement that tells the score when you win or when you lose -- losing will result in the computer score adding up
+      if (result == "You Won!") {
+         playerScoreboard = playerNum++
+      };
+
+      if (result == "You Lost!") {
+         pcScoreboard = pcNum++
+      }
+      document.querySelector('.playerScore').innerHTML = playerScoreboard;
+      document.querySelector('.computerScore').innerHTML = pcScoreboard;
       
    })
    
@@ -92,92 +115,3 @@
 
    
 })();
-
-
-
-// ------------------------------------------------------------------------------------------------------------------------------------------
-// ---------------------------------------------------------------    ISMI    --------------------------------------------------------------- 
-// ------------------------------------------------------------------------------------------------------------------------------------------
-
-/*
-   // option for computer
-   function getOptionComputer (){
-    const comp = Math.random();
-
-    if( comp < 0.34 )return'rock'; //if we used return they will stop and comeback to next value
-    if( comp >= 0.34 && comp < 0.67 ) return 'scissors';
-    return 'hand';
-    
-
-   }
-//adding rules in games  for getting the result (i choose return insted of else  )
-function  getResult(comp, player){
-     //i dont need var result cause i need return  
-    if( player == comp )return 'draw!';
-    if( player == 'rock' ) return ( comp == 'scissors' ) ? ' YOU WIN !' 
-    : ' YOU LOSE'; //i used this (:) IS mean apart from that
-    
-    if( player == 'scissors' ) return( comp == 'rock' ) ? ' YOU LOSE ' 
-    : 'YOU WIN!';
-    
-    if( player == 'hand' ) return ( comp == 'scissors' ) ? 'YOU LOSE! ' 
-    : 'YOU WIN!';
-    
-   
-}
- 
-// so i make  this rock button works by clicking and i used document.querySelector to call my class
-    const pRock = document.querySelector (".rock");
-    pRock.addEventListener('click', function(){
-    const optionComputer = getOptionComputer();
-    const optionPlayer = pRock.className //is calling class name from html
-    const result = getResult(optionComputer , optionPlayer);
-   
-    //console.log('comp :' + optionComputer);
-   //console.log('player :' + optionPlayer);
-   //console.log('anwser' : + result);
-
-   const imgComputer = document.querySelector(".img-komputer");
-   imgComputer.setAttribute('src' ,'img/' + optionComputer + '.png'); //i used this set attribute for change a picture for computer 
-   //bycalling class numbur and (img/  )is because i put my image in folder img
-
-   const info = document.querySelector('.info');
-   info.innerHTML = result;
-
-   
-
-});
-
-// so i make  this 2nd button for scissors
-    const pscissors = document.querySelector (".scissors");
-    pscissors.addEventListener('click', function(){
-    const optionComputer = getOptionComputer();
-    const optionPlayer = pscissors.className
-    const result = getResult(optionComputer , optionPlayer)
-
-   const imgComputer = document.querySelector(".img-komputer");
-   imgComputer.setAttribute('src' ,'img/' + optionComputer + '.png');
-
-   const info = document.querySelector('.info');
-   info.innerHTML = result;
-
-})
-    // and the last i make  this  button for hands
-
-    const phand = document.querySelector (".hand");
-    phand.addEventListener('click', function(){
-    const optionComputer = getOptionComputer();
-    const optionPlayer = phand.className
-    const result = getResult(optionComputer , optionPlayer)
-
-   const imgComputer = document.querySelector(".img-komputer");
-   imgComputer.setAttribute('src' ,'img/' + optionComputer + '.png');
-
-   const info = document.querySelector('.info');
-   info.innerHTML = result;
-
-
-}) 
-   
-// for 3 button i choose to make it one by one 
-*/
